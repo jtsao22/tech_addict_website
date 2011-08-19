387946780373021ea4199c25caa29eda24f3926a
Jason Tsao <jtsao22@gmail.com>
2011-08-19 02:49:15 -0700

    Added models for categories and posts. Began playing with google datastore

--- tech_blog/models.py
+++ tech_blog/models.py
@@ -1,16 +1,15 @@
-from appengine_django.models import BaseModel
+#from appengine_django.models import BaseModel
 from google.appengine.ext import db
-from django.db import models
+#from django.db import models
 
 # Create your models here.
 
-class Package(models.Model):
-    name        = models.CharField(maxlength=300)
-    version     = models.CharField(maxlength=300,
-                                   blank=True)
-    home_page   = models.URLField(blank=True)
-    summary     = models.TextField()
-    description = models.TextField(blank=True)
-    keywords    = models.TextField(blank=True)
-    categories  = models.ManyToManyField(Category,
-                                         related_name="packages")
+class Category(db.Model):
+    name = db.CategoryProperty()
+    posts = db.ListProperty(db.Key)
+
+class Post(db.Model):
+    title           = db.StringProperty()
+    slug            = db.LinkProperty()
+    body            = db.TextProperty()
+    published       = db.DateTimeProperty(auto_now=True)
--- tech_blog/views.py
+++ tech_blog/views.py
@@ -2,9 +2,23 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from tech_blog.models import *
 
 def index(request):
-    return render_to_response('index.html', {})
+
+    post = Post()
+    post.title = "Test Post"
+    post.body = "This is a test post"
+    #post.slug = "www.jtsao22.appspot.com";
+    post.put()
+
+    result = "";
+    posts = Post.all()
+
+    for p in posts.fetch(limit=40):
+        result += p.title + " has a body of: " + p.body + "\n"
+
+    return render_to_response('index.html', {'result': result})
 
 def error(request):
     return HttpResponse("<p> Page not found </p>")
